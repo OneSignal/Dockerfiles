d27f9856f6f8dd53ff715c6bdc13fca5c8d72142
Nate Mara <nate@onesignal.com>
2020-08-28 11:28:22 -0700

    Fix single version deployment script

--- deploy-single-version.sh
+++ deploy-single-version.sh
@@ -5,5 +5,5 @@ set -e
 version=$1
 
 ./deploy-single.sh "${version}"
-./deploy-single.sh "${version}-openssl-1.1"
 ./deploy-single.sh "${version}-ruby-capnp"
+./deploy-single.sh "${version}-openssl-1.1"
