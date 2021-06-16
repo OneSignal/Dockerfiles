bc673bdd820d2efd967105442dbd86c3c5df1092
Niels Stevens <niels.stevens@onesignal.com>
2021-06-16 14:27:13 +0200

    fixup! feat: add Barman image for psql backups

--- barman/entrypoint.sh
+++ barman/entrypoint.sh
@@ -6,8 +6,6 @@ echo ">>> Checking all configurations"
 [[ "$DB_USER" != "" ]] || ( echo 'Variable DB_USER is not set!' ;exit 2 )
 [[ "$DB_PASSWORD" != "" ]] || ( echo 'Variable DB_PASSWORD is not set!' ;exit 3 )
 [[ "$DB_NAME" != "" ]] || ( echo 'Variable DB_NAME is not set!' ;exit 4 )
-[[ "$DB_STREAMING_USER" != "" ]] || ( echo 'Variable DB_USER is not set!' ;exit 5 )
-#[[ "$DB_STREAMING_PASSWORD" != "" ]] || ( echo 'Variable DB_PASSWORD is not set!' ;exit 6 )
 
 BACKUP_RETENTION_DAYS=${BACKUP_RETENTION_DAYS:-1}
 BACKUP_SCHEDULE=${BACKUP_SCHEDULE:-'0 0 * * *'}
@@ -24,7 +22,7 @@ streaming_archiver = on
 streaming_archiver_name = barman_receive_wal
 streaming_archiver_batch_size = 50
 conninfo = host=$DB_HOST dbname=$DB_NAME user=$DB_USER password=$DB_PASSWORD port=$DB_PORT connect_timeout=$DB_CONNECTION_TIMEOUT application_name=barman
-streaming_conninfo = host=$DB_HOST user=$DB_STREAMING_USER password=$DB_PASSWORD port=$DB_PORT
+streaming_conninfo = host=$DB_HOST user=$DB_USER password=$DB_PASSWORD port=$DB_PORT
 backup_directory = $BACKUP_DIR
 slot_name = barman
 retention_policy = RECOVERY WINDOW OF $BACKUP_RETENTION_DAYS DAYS
@@ -36,6 +34,8 @@ echo "$BACKUP_SCHEDULE /usr/bin/barman backup all > /proc/1/fd/1 2> /proc/1/fd/2
 
 echo '>>> STARTING CRON'
 env >> /barman/environment
+
+# TODO: before starting our cron we need to start `barman receive-wal pg_cluster` in the background
 
 if [[ "$DEBUG" = 'true' ]]; then
   sed -i 's/INFO/DEBUG/g' /etc/barman.conf
